8ff43f962ecccdc67d90e1d9c9a046e528f13cda
lookmhen <lookmhen@gmail.com>
2024-02-05 22:38:45 +0700

    new project

---
+++ VowelCounting/app.js
@@ -0,0 +1,13 @@
+const countEL = document.querySelector(".count");
+const input = document.querySelector("input");
+
+input.addEventListener("keyup", () => {
+  let word = input.value.toLowerCase();
+  let vowelCount = 0;
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    if(letter.match(/([a,e,i,o,u])/))
+        vowelCount++
+  }
+  countEL.innerHTML=`${vowelCount}`
+});
